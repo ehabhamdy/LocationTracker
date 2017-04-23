e98b59007ed03bcaacb81d3fad0a02c27012822d
ehabhamdy <ehabhamdy2012@gmail.com>
2017-04-23 17:14:15 +0200

    Adding pubnup tracking activity

---
+++ app/src/main/java/com/realtime/ehabhamdy/locationtracker/PubnupPublisherActivity.java
@@ -0,0 +1,13 @@
+package com.realtime.ehabhamdy.locationtracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PubnupPublisherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_publisher);
+    }
+}
